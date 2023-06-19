f072ec97aa38c12772f61d09f70094f0ce82d1f7
Sergii <sergiyomelchuk@outlook.com>
2023-06-19 17:42:34 +0300

    updated index.js
    без CSS, без іншого офрмлення.
    Розмітку з js переніс в html. По іншому не працює, не знаю чому.

--- src/index.js
+++ src/index.js
@@ -10,28 +10,67 @@ const refs = {
   breedTemperament: document.querySelector('.breed-temperament'),
 };
 
-refs.infoContainer.insertAdjacentHTML(
-  'afterbegin',
-  `<img class="cat-image" alt="Cat Image">
-         <div class="cat-info-style">
-          <h1 class="breed-name"></h1>
-          <p class="breed-description"></p>
-          <p class="breed-temperament"></p>
-    </div>`
-);
+// refs.infoContainer.insertAdjacentHTML(
+//   'afterbegin',
+//   `<img class="cat-image" alt="Cat Image">
+//          <div class="cat-info-style">
+//           <h1 class="breed-name"></h1>
+//           <p class="breed-description"></p>
+//           <p class="breed-temperament"></p>
+//     </div>`
+// );
 
-fetchBreeds().then(breeds => {
-  if (breeds.length > 0) {
-    console.log(breeds);
-    const options = breeds.map(breed => {
-      const element = document.createElement('option');
-      element.value = breed.id;
-      element.textContent = breed.name;
-      return element;
-    });
+fetchBreeds()
+  .then(breeds => {
+    if (breeds.length > 0) {
+      console.log(breeds);
+      const options = breeds.map(breed => {
+        const element = document.createElement('option');
+        element.value = breed.id;
+        element.textContent = breed.name;
+        return element;
+      });
 
-    refs.breedSelect.append(...options);
-  } else {
-    console.log('No breeds found');
-  }
-});
+      refs.breedSelect.append(...options);
+    } else {
+      console.log('No breeds found');
+    }
+  })
+  .catch(() => {
+    console.log('Oops! Something went wrong! Try reloading the page!');
+  })
+  .finally(() => {});
+
+refs.breedSelect.addEventListener('change', showSelectedDbreed);
+
+function showSelectedDbreed() {
+  const breedSelectedById = refs.breedSelect.value;
+  fetchCatByBreed(breedSelectedById)
+    .then(element => {
+      if (element.length > 0) {
+        console.log('work');
+        const catInfo = element[0];
+        console.log(catInfo);
+        const catBreed = catInfo.breeds[0];
+        console.log(catBreed);
+        console.log(catInfo.url);
+
+        refs.catImg.src = catInfo.url;
+        console.log(catInfo.url);
+        refs.breedName.textContent = catBreed.name;
+        console.log(catBreed.name);
+        refs.breedDescription.textContent = catBreed.breedDescription;
+        console.log(catBreed.breedDescription);
+        refs.breedTemperament.textContent = catBreed.temperament;
+        console.log(catBreed.temperament);
+
+        refs.infoContainer.style.display = 'flex';
+      } else {
+        console.log('No cat found for the given breed ID');
+      }
+    })
+    .catch(() => {
+      console.log('Oops! Something went wrong! Try reloading the page!');
+    })
+    .finally(() => {});
+}
